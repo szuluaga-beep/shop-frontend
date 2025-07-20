2446f63c606b2430797873180c15b25d21b817f8
szuluaga-beep <steven000069@hotmail.com>
2025-07-20 10:59:56 -0500

    feat: add Checkout component and integrate into FormPayment

---
+++ src/components/checkout.tsx
@@ -0,0 +1,64 @@
+import { Button, Drawer, DrawerBody, DrawerContent, DrawerFooter, DrawerHeader, useDisclosure } from "@heroui/react";
+
+
+export const Checkout = () => {
+    const { isOpen, onOpen, onOpenChange } = useDisclosure();
+    return (
+        <>
+            <Button onPress={onOpen}>Open Drawer</Button>
+            <Drawer placement="bottom" size="5xl" isOpen={isOpen} onOpenChange={onOpenChange}
+                motionProps={{
+                    variants: {
+                        enter: {
+                            opacity: 1,
+                            x: 0,
+                            dur: 0.3,
+                            
+                        },
+                        exit: {
+                            x: 100,
+                            opacity: 0,
+                            dur: 0.3,
+                        },
+                    },
+                }}
+            >
+                <DrawerContent>
+                    {(onClose) => (
+                        <>
+                            <DrawerHeader className="flex flex-col gap-1">Drawer Title</DrawerHeader>
+                            <DrawerBody>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pulvinar risus non
+                                    risus hendrerit venenatis. Pellentesque sit amet hendrerit risus, sed porttitor
+                                    quam.
+                                </p>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pulvinar risus non
+                                    risus hendrerit venenatis. Pellentesque sit amet hendrerit risus, sed porttitor
+                                    quam.
+                                </p>
+                                <p>
+                                    Magna exercitation reprehenderit magna aute tempor cupidatat consequat elit dolor
+                                    adipisicing. Mollit dolor eiusmod sunt ex incididunt cillum quis. Velit duis sit
+                                    officia eiusmod Lorem aliqua enim laboris do dolor eiusmod. Et mollit incididunt
+                                    nisi consectetur esse laborum eiusmod pariatur proident Lorem eiusmod et. Culpa
+                                    deserunt nostrud ad veniam.
+                                </p>
+                            </DrawerBody>
+                            <DrawerFooter>
+                                <Button color="danger" variant="light" onPress={onClose}>
+                                    Close
+                                </Button>
+                                <Button color="primary" onPress={onClose}>
+                                    Action
+                                </Button>
+                            </DrawerFooter>
+                        </>
+                    )}
+                </DrawerContent>
+            </Drawer>
+        </>
+
+    )
+}
